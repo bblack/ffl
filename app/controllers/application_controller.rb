67f536e70900f2ca1101a415f30f4f395151af7d
Brian Black <black@gatech.edu>
2012-02-02 21:05:42 -0500

    Added set_current_user filter and @current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_current_user
   before_filter :reject_posts_by_nongods
+  
+  def set_current_user
+    if session[:user_id].nil?
+      @current_user = nil
+    else
+      @current_user = User.find(session[:user_id])
+    end
+  end
   
   def reject_posts_by_nongods
     errors = []
