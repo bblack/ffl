293796828fe90ec958044ec73600a6b250b9f215
Brian Black <black@gatech.edu>
2012-02-04 22:06:29 -0500

    Contracts spreadsheet

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,5 +1,26 @@
 class ContractsController < ApplicationController
   
+  def index
+    render :text => "No league selected" if @current_league.nil?
+    contracts = @current_league.contracts.includes(:player, :team)
+    retval = "Player,Position,Team,Owner,Contract value,Contract start,Contract length,Contract end"
+    retval += "\r\n"
+    contracts.each do |c|
+      retval += [
+        "#{c.player.first_name} #{c.player.last_name}",
+        c.player.position,
+        c.player.nfl_team,
+        c.team.name.gsub(',',' '),
+        c.value,
+        c.first_year,
+        c.length,
+        c.first_year.nil? || c.length.nil? ? nil : c.first_year + c.length - 1
+      ] * ','
+      retval += "\r\n"
+    end
+    send_data retval, :type => 'application/csv', :filename => "#{@current_league.name}_#{Time.now.iso8601}.csv"
+  end
+  
   def create
     @contract = Contract.create(
       :team_id => params[:team_id],
