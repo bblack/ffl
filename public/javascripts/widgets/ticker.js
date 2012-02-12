b8ced7f29ae73e5a3d949264e9dd4dcbe0c0f187
Brian Black <black@gatech.edu>
2012-02-12 12:18:46 -0500

    Added ESPN styling to ticker (except font)
    ESPN ticker images (squash into last)

--- public/javascripts/widgets/ticker.js
+++ public/javascripts/widgets/ticker.js
@@ -7,10 +7,31 @@ $.widget('ffl.ticker', {
   },
 
   _create: function() {
-    this.span1 = $("<span class='tickerspan' />");
-    this.span1.text(this.options.text);
     this.element.empty();
+    
+    img_back = $("<img src='/images/ticker-back.png' style='position:absolute;' />");
+    img_back.css('top', 0);
+    img_back.css('left', 0);
+    img_back.css('height', this.element.innerHeight());
+    img_back.css('width', '100%');
+    this.element.append(img_back);
+    
+    this.span1 = $("<span class='tickerspan' />");
     this.element.append(this.span1);
+    
+    img_right = $("<img src='/images/ticker-right.png' style='position:absolute;' />");
+    img_right.css('top', 0);
+    img_right.css('right', 0);
+    img_right.css('height', this.element.innerHeight());
+    this.element.append(img_right);
+    debugger;
+    
+    img_left = $("<img src='/images/ticker-left.png' style='position:absolute;' />");
+    img_left.css('top', 0);
+    img_left.css('left', 0);
+    img_left.css('height', this.element.innerHeight());
+    this.element.append(img_left);
+    
     this._resetPosition();
     this._tick();
   },
