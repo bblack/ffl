fbf8449b3de735616fe2d35966a89afe582e0f0b
Brian Black <black@gatech.edu>
2012-03-01 11:03:00 -0500

    Refactored english-friendly timespan string function Added notice of time remaining in RFA decision period on RFA show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,12 @@ module ApplicationHelper
   def brian?
     @current_user and (@current_user.name == 'brian' or @current_user.god_mode)
   end
+
+  def timespan_string(seconds_left)
+      time_left_days = (seconds_left / (60*60*24)).to_i
+      time_left_hours = ((seconds_left - time_left_days*60*60*24) / (60*60)).to_i
+      time_left_minutes = ((seconds_left - time_left_days*60*60*24 - time_left_hours*60*60) / 60).to_i
+      return "#{time_left_days} days, #{time_left_hours} hours, #{time_left_minutes} minutes"
+  end
   
 end
