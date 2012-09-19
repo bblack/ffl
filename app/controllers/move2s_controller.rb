1f2f2acdbc267d2d841f3440d23ebab6e499575d
Brian Black <black@gatech.edu>
2012-09-19 02:47:44 -0400

    Set 1 to new pv for the first move for a player in a league

--- app/controllers/move2s_controller.rb
+++ app/controllers/move2s_controller.rb
@@ -2,7 +2,12 @@ class Move2sController < ApplicationController
   def create
     m = Move2.new(params.slice(:player_id, :new_team_id, :league_id, :new_pv, :final_year))
     last_move = Move2.where(params.slice(:player_id, :league_id)).order(:id).last
-    m.old_team_id = last_move ? last_move.new_team_id : nil
+    if last_move
+      m.old_team_id = last_move.new_team_id
+    else
+      m.new_pv = 1
+    end
+    
     m.save!
 
     redirect_to :back
