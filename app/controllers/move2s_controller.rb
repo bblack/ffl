ad957affb77d39dda13fd2469b63c72df26a58a5
Brian Black <black@gatech.edu>
2012-09-19 01:14:50 -0400

    quick bugfix

--- app/controllers/move2s_controller.rb
+++ app/controllers/move2s_controller.rb
@@ -1,6 +1,6 @@
 class Move2sController < ApplicationController
   def create
-    m = Move2.new(params.slice(:player_id, :new_team_id, :league_id))
+    m = Move2.new(params.slice(:player_id, :new_team_id, :league_id, :new_pv))
     last_move = Move2.where(params.slice(:player_id, :league_id)).order(:id).last
     m.old_team_id = last_move ? last_move.new_team_id : nil
     m.save!
