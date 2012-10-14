92936bea56bcf14d3d4f67d9f7240af7ec698b36
Brian Black <black@gatech.edu>
2012-10-14 18:37:06 -0400

    Move2 must have player_id present

--- app/models/move2.rb
+++ app/models/move2.rb
@@ -4,6 +4,8 @@ class Move2 < ActiveRecord::Base
   belongs_to :old_team, :class_name => "Team", :foreign_key => "old_team_id"
   belongs_to :new_team, :class_name => "Team", :foreign_key => "new_team_id"
 
+  validates :player_id, :presence => true
+
   validates_each :old_team, :new_team do |model, att, value|
     model.errors.add(att, 'must belong to league') unless value.nil? || value.league_id == model.league_id
   end
