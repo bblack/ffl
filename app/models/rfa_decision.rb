26ca0b298ad7318e9d0ab0b71472bcd4d1a62e3a
Brian Black <black@gatech.edu>
2013-04-09 19:45:38 -0400

    Validate that decision period is open on decision creation

--- app/models/rfa_decision.rb
+++ app/models/rfa_decision.rb
@@ -3,11 +3,18 @@ class RfaDecision < ActiveRecord::Base
   belongs_to :player
   belongs_to :rfa_decision_period
   validate :validate_team_has_player
+  validate :rfa_decision_period_is_open, :on => :create
 
   def validate_team_has_player
     contract = self.team.players_pvcs.where(:player_id => self.player_id)
     if contract.none?
       errors.add(:team_id, "must be the same as the team who owns this player's contract in this league.")
+    end
+  end
+
+  def rfa_decision_period_is_open
+    if not rfa_decision_period.open?
+      errors.add(:rfa_decision_period, "must be open")
     end
   end
 
