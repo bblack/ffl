c1a43aac5db72a10b8b131a4bafc8f26f40bad84
Brian Black <black@gatech.edu>
2013-04-09 23:05:53 -0400

    fuck this

--- app/models/player_value_change.rb
+++ app/models/player_value_change.rb
@@ -1,4 +1,5 @@
 class PlayerValueChange < ActiveRecord::Base
   belongs_to :player
   belongs_to :team # The team who signed the contract causing the value change. nil if a drop (i.e. new_value == nil)
+  validates :team, :presence => true # Must exist in the right league. consider changing to league_id
 end
