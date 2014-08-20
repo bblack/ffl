e22b84633b9383f4705af789eafa6689143cbb3f
Brian Black <black@gatech.edu>
2014-08-20 12:20:35 -0400

    PVC#length

--- app/models/player_value_change.rb
+++ app/models/player_value_change.rb
@@ -2,4 +2,12 @@ class PlayerValueChange < ActiveRecord::Base
   belongs_to :player
   belongs_to :team # The team who signed the contract causing the value change. nil if a drop (i.e. new_value == nil)
   validates :team, :presence => true # Must exist in the right league. consider changing to league_id
+
+  def length
+    if last_year && first_year
+      last_year - first_year + 1
+    else
+      nil
+    end
+  end
 end
