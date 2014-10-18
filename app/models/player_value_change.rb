1f4ca4ae616c06b95979c9ee6d20e4ee749b323c
Brian Black <black@gatech.edu>
2014-10-18 01:37:50 -0400

    pvc no longer belongs_to :team

--- app/models/player_value_change.rb
+++ app/models/player_value_change.rb
@@ -1,7 +1,6 @@
 class PlayerValueChange < ActiveRecord::Base
   belongs_to :player
-  belongs_to :team # The team who signed the contract causing the value change. nil if a drop (i.e. new_value == nil)
-  validates :team, :presence => true # Must exist in the right league. consider changing to league_id
+  belongs_to :league
 
   def length
     if last_year && first_year
