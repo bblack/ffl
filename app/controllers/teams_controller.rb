52f5736d611fe342591c97adcc44a0da046f8672
Brian Black <black@gatech.edu>
2014-10-19 21:53:11 -0400

    don't make dozens of queries on team index page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,6 +3,33 @@ class TeamsController < ApplicationController
 
   def index
     change_current_league(params[:league_id])
+
+    pvcs = @current_league.signed_players_pvcs.all
+    rosters = EspnRosterSpot.where(:team_id => @current_league.team_ids).includes(:player).all.group_by(&:team_id)
+
+    @rosters_by_pos = {}
+
+    by_player = {}
+
+    rosters.each do |tid, roster|
+      roster.each do |spot|
+        by_player[spot.player.id] = {spot: spot}
+        @rosters_by_pos[tid] ||= {}
+        @rosters_by_pos[tid][spot.player.position] ||= 0
+        @rosters_by_pos[tid][spot.player.position] += 1
+      end
+    end
+
+    pvcs.each do |pvc|
+      by_player[pvc.player_id][:pvc] = pvc
+    end
+
+    @payrolls = {}
+
+    by_player.each do |pid, stuff|
+      @payrolls[stuff[:spot].team_id] ||= 0
+      @payrolls[stuff[:spot].team_id] += stuff[:pvc].new_value
+    end
   end
 
   def fetch_espn
