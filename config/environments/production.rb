f1597dd753d96abc06975114f024a122f80614d9
Brian Black <black@gatech.edu>
2016-11-22 19:49:15 -0500

    try to get the fucking assets to compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,10 @@ Ffl::Application.configure do
   config.assets.compress = true
 
   # Choose the compressors to use
-  # config.assets.js_compressor  = :uglifier
+  config.assets.js_compressor  = Closure::Compiler.new(
+    language_in: 'ECMASCRIPT6',
+    language_out: 'ES5'
+  )
   # config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
@@ -65,4 +68,5 @@ Ffl::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile = [ Proc.new{ |path| !File.extname(path).in?(['.js', '.css', '.map', '.gzip']) }, /application.(css|js)$/ ]
 end
